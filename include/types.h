0776248ab79d92aee8eb91607eb5100fc1bc2718
hanngo416 <hanngo416@gmail.com>
2026-04-28 23:33:14 +0700

    feat: add utility functions for input validation and string manipulation

--- include/types.h
+++ include/types.h
@@ -4,25 +4,19 @@
 #include <time.h>
 
 #define MAX_VEHICLES 3636
-
-// Enum cho loại xe
 typedef enum {
     MOTO = 0,
     CAR = 1,
     BUS = 2
 } VehicleType;
-
-// Struct thông tin phương tiện
 typedef struct {
     char licensePlate[12];
     VehicleType type;
     time_t entryTime;
     time_t exitTime;
     double fee;
-    int status; // 0: Đang đỗ, 1: Đã ra
+    int status;
 } Vehicle;
-
-// Struct bảng giá đỗ xe
 typedef struct {
     VehicleType type;
     char typeName[20];
---
+++ include/utils.h
@@ -0,0 +1,27 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include "types.h"
+int getInt(const char *messageInfo,const char *messageErrorOutOfRange,const char *messageErrorNumber,int min, int max);
+
+int getIntUpdate(const char *messageInfo,const char *messageErrorOutOfRange,const char *messageErrorNumber,int min, int max,int oldValue);
+
+double getDouble(const char *messageInfo,const char *messageError,double min);
+
+void getString(const char *messageInfo,char *buffer,int maxLen);
+
+void getStringUpdate(const char *messageInfo,char *buffer,int maxLen,const char *oldValue);
+void toUpperCase(char *str);
+int isEmpty(const char *str);
+
+int isValidLicensePlate(const char *plate);
+int isDuplicateLicensePlate(Vehicle vehicles[], int count, const char *plate);
+
+int isValidVehicleType(int type);
+int isValidStatus(int status);
+
+int parseDateTime(const char *dateStr, time_t *result);
+int isValidEntryExitTime(time_t entryTime, time_t exitTime)
+int isValidEntryTime(time_t entryTime)
+int isValidUsername(const char *username);
+int isValidPassword(const char *password);
+#endif
