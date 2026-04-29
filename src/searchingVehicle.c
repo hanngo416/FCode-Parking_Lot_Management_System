3f66da932be2dc32c0d5f0d5d28e697219cd60c4
Scholarly2 <125072829+Scholarly2@users.noreply.github.com>
2026-04-29 22:16:36 +0700

    Update searchVehicle function for better readability

--- src/searchingVehicle.c
+++ src/searchingVehicle.c
@@ -9,12 +9,12 @@ extern int total_vehicles;
 void searchVehicle() {
     char key[15];
     int found = 0;
-    printf("\n\033[1;4;36m======= TIM KIEM BIEN SO =======\033[0m\n");
-    printf("Nhap ky tu can tim >> ");
+    printf("\n\033[1;4;36m======= SEARCHING VEHICLE =======\033[0m\n");
+    printf("Enter search keyword >> ");
     scanf("%s", key);
 
     printf("\n\033[1;37m%-5s | %-15s | %-10s | %-25s | %-12s\033[0m\n", 
-           "STT", "BIEN SO", "LOAI XE", "GIO VAO", "TRANG THAI");
+           "STT", "LICENSE PLATE", "VEHICLE TYPE", "ENTRY TIME", "STATUS");
     printf("------------------------------------------------------------------------------------------\n");
 
     for (int i = 0; i < total_vehicles; i++) {
@@ -22,11 +22,11 @@ void searchVehicle() {
             found++;
             
             char *typeStr;
-            if (list[i].type == MOTO) typeStr = "Xe may";
-            else if (list[i].type == CAR) typeStr = "O to";
-            else if (list[i].type == BUS) typeStr = "Xe buyt";
-            else typeStr = "Khac";
-            
+            if (list[i].type == MOTO) typeStr = "Motocycle";
+            else if (list[i].type == CAR) typeStr = "Car";
+            else if (list[i].type == BUS) typeStr = "Bus";
+            else typeStr = "Other";
+
             char timeStr[26];
             char *rawTime = ctime(&list[i].entryTime);
             if (rawTime != NULL) {
@@ -38,8 +38,8 @@ void searchVehicle() {
 
             printf("%-5d | %-15s | %-10s | %-25s | %s\n", 
                    found, list[i].licensePlate, typeStr, timeStr,
-                   (list[i].status == 0 ? "\033[1;32mTrong bai\033[0m" : "\033[1;31mDa ra\033[0m"));
+                   (list[i].status == 0 ? "\033[1;32mCurrently parked\033[0m" : "\033[1;31mAlready exited\033[0m"));
         }
     }
-    if (found == 0) printf("\033[1;31m[LOI] Khong tim thay xe!\033[0m\n");
+    if (found == 0) printf("\033[1;31m[LOI] Vehicle not found!\033[0m\n");
 }
