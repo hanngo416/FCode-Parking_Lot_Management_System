822b5b50b2b9d3ca96e8ed8b4cc1fc2ea6e1ea25
TTTvy.0701 <tttbeo.007@gmail.com>
2026-05-15 22:05:47 +0700

    update bill

--- src/billing.c
+++ src/billing.c
@@ -15,24 +15,87 @@ double calculateFee(Vehicle v, ParkingLot *p) {
     double fee = hours * p->prices[v.type].price;
 
     if (hours > 72)
-        printf("\n[ADMIN WARNING] Vehicle parked over 72 hours!\n");
+        printf("\n[WARNING] Vehicle parked over 72 hours!\n");
 
     return fee;
 }
 
-void printBill(Vehicle v) {
+void printBill(Vehicle v, ParkingLot *p) {
+
     const char *typeStr;
-    if      (v.type == MOTO) typeStr = "Motorbike";
-    else if (v.type == CAR)  typeStr = "Car";
-    else                     typeStr = "Bus";
+
+    if (v.type == MOTO)
+        typeStr = "Motorbike";
+    else if (v.type == CAR)
+        typeStr = "Car";
+    else
+        typeStr = "Bus";
+
+    double parkingHours =
+        difftime(v.exitTime, v.entryTime) / 3600.0;
+
+    int roundedHours = (int)(parkingHours + 0.5);
+
+    double baseFee = v.fee;
+    double surchargePerHour = 5000;
+    double overtimeSurcharge = 0;
+    double nightSurchargeFee = 0;
+    int nightSurchargeHours = 0;
+
+    time_t tempTime = v.entryTime;
+
+    while (tempTime < v.exitTime) {
+
+        struct tm *local = localtime(&tempTime);
+        int hour = local->tm_hour;
+
+        if (hour >= 23 || hour < 3) {
+            nightSurchargeFee += surchargePerHour;
+            nightSurchargeHours++;
+        }
+        tempTime += 3600;
+
+        if (parkingHours > 72) {
+            overtimeSurcharge = 50000;
+        }
+    }
+
+    double totalFee = baseFee + nightSurchargeFee + overtimeSurcharge;
 
     printf("\n===== BILL =====\n");
-    printf("Plate : %s\n",   v.licensePlate);
-    printf("Type  : %s\n",   typeStr);
-    printf("Entry : %s",     ctime(&v.entryTime));
-    printf("Exit  : %s",     ctime(&v.exitTime));
-    printf("Fee   : %.0f VND\n", v.fee);
+
+    printf("Plate              : %s\n", v.licensePlate);
+    printf("Type               : %s\n", typeStr);
+
+    printf("Entry Time         : %s", ctime(&v.entryTime));
+    printf("Exit Time          : %s", ctime(&v.exitTime));
+
+    printf("Parking Hours      : %d hours\n", roundedHours);
+
+    printf("Night Parking Time : %d hours\n", nightSurchargeHours);
+
+    printf("Base Fee           : ");
+    printMoney(baseFee);
+    printf("\n");
+
+    printf("Night Hours Fee (23h-3h)   : %.0f VND/hour\n",
+       surchargePerHour);
+
+    printf("Night Surcharge   : ");
+    printMoney(nightSurchargeFee);
+    printf("\n");
+
+    printf("72h Surcharge     : ");
+    printMoney(overtimeSurcharge);
+    printf("\n");
+
+    printf("Total Fee          : ");
+    printMoney(totalFee);
+    printf("\n");
+
+    printf("========================\n");
 }
+
 
 void updatePrice(ParkingLot *p) {
     printf("\n===== UPDATE PRICE =====\n");
